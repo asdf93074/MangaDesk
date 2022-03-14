1dd05c9260a0c1d53f04e3870d0a36c38d6d8b12
Mahad Khan <mahadkhan2009@hotmail.com>
2022-03-14 20:23:00 +0500

    Refactoring to mangadex API and improved fetch manga by id to also get cover url.

--- src/api/extensions/mangadex.api.ts
+++ src/api/extensions/mangadex.api.ts
@@ -8,22 +8,9 @@ export class MangaDexAPI implements MangaAPI {
 	a = a;
 
 	populateHome = (offset: number): Promise<Manga[]> => {
-		let mangas: Manga[] = [];
 		return axios.get(`${this.baseUrl}/manga?limit=${30}&offset=${offset}&includes[]=cover_art`)
 			.then((res) => {
-				mangas = (res.data.data as []).map((d: any) => {
-					const coverFileName: string = ((d.relationships as []).find((rel: any) => rel.type === 'cover_art') as any).attributes.fileName;
-
-					return {
-						name: d.attributes.title.en || d.attributes.altTitles[0],
-						id: d.id,
-						coverUrl: `https://uploads.mangadex.org/covers/${d.id}/${coverFileName}.256.jpg`,
-						description: d.attributes.description.en || d.attributes.description[0],
-						tags: (d.attributes.tags as []).filter((tag: any) => tag.attributes.group === 'genre').map((tag: any) => tag.attributes.name.en),
-					};
-				});
-
-				return mangas;
+				return (res.data.data as []).map((d: any) => mapResponseToMangaObject(d));
 			})
 			.catch((res) => {
 				console.error('Something went wrong while fetching manga.', res);
@@ -32,23 +19,25 @@ export class MangaDexAPI implements MangaAPI {
 	};
 
 	fetchMangaById = (id: string): Promise<Manga> => {
-		return axios.get(`${this.baseUrl}/manga/${id}`)
+		return axios.get(`${this.baseUrl}/manga/${id}?includes[]=cover_art`)
 			.then((res) => {
-				const d = res.data.data;
-
-				const data: Manga = {
-					name: d.attributes.title.en || d.attributes.altTitles[0],
-					id: d.id,
-					coverUrl: null,
-					description: d.attributes.description.en || d.attributes.description[0],
-					tags: (d.attributes.tags as []).filter((tag: any) => tag.attributes.group === 'genre').map((tag: any) => tag.attributes.name.en),
-				};
-
-				return data;
+				return mapResponseToMangaObject(res.data.data);
 			})
 			.catch((res) => {
 				console.error(`Something went wrong while fetching manga ${id}.`, res);
 				return null;
 			});
+	};
+}
+
+function mapResponseToMangaObject(manga: any): Manga {
+	const coverFileName: string = ((manga.relationships as []).find((rel: any) => rel.type === 'cover_art') as any).attributes.fileName;
+
+	return {
+		name: manga.attributes.title.en || manga.attributes.altTitles[0],
+		id: manga.id,
+		coverUrl: `https://uploads.mangadex.org/covers/${manga.id}/${coverFileName}.256.jpg`,
+		description: manga.attributes.description.en || manga.attributes.description[0],
+		tags: (manga.attributes.tags as []).filter((tag: any) => tag.attributes.group === 'genre').map((tag: any) => tag.attributes.name.en),
 	};
 }
