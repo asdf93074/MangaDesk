55427c73eb2d07ac8e591e03c171df844d7c7c19
Mahad Khan <mahadkhan2009@hotmail.com>
2022-08-08 22:40:41 +0500

    replace webpack with vite and remove unnecessary packages and files.

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      './': path.resolve('./src/'),
+      'src': path.resolve('./src'),
+      'api': path.resolve('./src/api'),
+      'app': path.resolve('./src/app'),
+      'models': path.resolve('./src/models'),
+      'utils': path.resolve('./src/utils'),
+      './': path.resolve('./src/constants'),
+      'features': path.resolve('./src/features'),
+      'components': path.resolve('./src/components'),
+    },
+  },
+  plugins: [react()],
+  clearScreen: true,
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@import "src/constants/_global.sass"\n`,
+      },
+    },
+  },
+});
